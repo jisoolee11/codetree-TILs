221eab4be9270b562fcbbc16d089982c7349536a
Jisoo Lee <63850508+jisoolee11@users.noreply.github.com>
2024-02-25 14:31:41 +0900

    Title: 바이러스 검사, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240225/바이러스 검사/virus-detector.py
@@ -0,0 +1,13 @@
+n = int(input())
+cust = list(map(int, input().split()))
+ldr_max, mbr_max = map(int, input().split())
+total = 0
+
+mbr_cust = list(map(lambda x: x-ldr_max, cust))
+total += len(cust)
+
+for c in mbr_cust:
+    if c > 0:
+        total += c // mbr_max + 1
+
+print(total)
