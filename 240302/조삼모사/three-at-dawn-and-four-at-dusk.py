7dd0be3b8545a287eaa54f98f7c35f8034121db0
Jisoo Lee <63850508+jisoolee11@users.noreply.github.com>
2024-03-02 18:04:27 +0900

    Title: 조삼모사, Time: 1603ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240302/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 240302/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -3,35 +3,37 @@ def calc():
     evening_work = 0
     for i in range(n):
         for j in range(n):
-            if i in morning_works_idx and j in morning_works_idx:
+            if not evening[i] and not evening[j]:
                 morning_work += works[i][j]
-            elif i not in morning_works_idx and j not in morning_works_idx:
+            elif evening[i] and evening[j]:
                 evening_work += works[i][j]
 
     return abs(morning_work - evening_work)
 
 
-def dfs(i):
+def dfs(i, cnt):
     global result
 
-    if len(morning_works_idx) == n // 2:
+    if cnt == n // 2:
         result = min(calc(), result)
         return
+
+    if i == n:
+        return
     
-    for i in range(i, n):
-        if not visited[i]:
-            visited[i] = True
-            morning_works_idx.append(i)
-            dfs(i+1)
-            visited[i] = False
-            morning_works_idx.pop()
+    dfs(i+1, cnt)
+
+    evening[i] = True
+    dfs(i+1, cnt+1)
+    evening[i] = False
+
 
 
 n = int(input())
 works = [list(map(int, input().split())) for _ in range(n)]
-visited = [False] * n
-morning_works_idx = []
+evening = [False] * n
+
 result = 100
 
-dfs(0)
+dfs(0, 0)
 print(result)
