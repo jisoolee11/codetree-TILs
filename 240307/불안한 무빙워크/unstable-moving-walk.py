746b6f0a5375ba3ebbda0bf87b13a4dd5b419815
Jisoo Lee <63850508+jisoolee11@users.noreply.github.com>
2024-03-07 11:45:04 +0900

    Title: 불안한 무빙워크, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240307/불안한 무빙워크/unstable-moving-walk.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def moving_rotate():
+    moving_safe.rotate()
+    for i in range(len(person)):
+        person[i] += 1
+
+
+def moving_person():
+    for idx, i in enumerate(person):
+        if i+1 not in person and moving_safe[i+1] != 0:
+            person[idx] = i+1
+            moving_safe[i+1] -= 1
+            if n-1 in person:
+                person.remove(n-1)
+
+
+def new_person():
+    if 0 not in person and moving_safe[0] != 0:
+        person.append(0)
+        moving_safe[0] -= 1
+
+
+n, k = map(int, input().split())
+moving_safe = deque(map(int, input().split()))
+person = []
+
+cnt = 0
+while moving_safe.count(0) < k:
+    cnt += 1
+    moving_rotate()
+    moving_person()
+    new_person()
+
+print(cnt)
