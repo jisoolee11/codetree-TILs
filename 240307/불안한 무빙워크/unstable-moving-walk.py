f3da14a6c6d70ee0ae2c898bc690b4538d4ca469
Jisoo Lee <63850508+jisoolee11@users.noreply.github.com>
2024-03-07 11:54:32 +0900

    Title: 불안한 무빙워크, Time: 617ms, Memory: 27MB, Status: Passed - CodeTree

--- 240307/불안한 무빙워크/unstable-moving-walk.py
+++ 240307/불안한 무빙워크/unstable-moving-walk.py
@@ -1,9 +1,17 @@
 from collections import deque
 
+
+def person_remove():
+    if n-1 in person:
+        person.remove(n-1)
+
+
 def moving_rotate():
     moving_safe.rotate()
     for i in range(len(person)):
         person[i] += 1
+
+    person_remove()
 
 
 def moving_person():
@@ -11,14 +19,16 @@ def moving_person():
         if i+1 not in person and moving_safe[i+1] != 0:
             person[idx] = i+1
             moving_safe[i+1] -= 1
-            if n-1 in person:
-                person.remove(n-1)
+
+    person_remove()
 
 
 def new_person():
     if 0 not in person and moving_safe[0] != 0:
         person.append(0)
         moving_safe[0] -= 1
+
+    person_remove()
 
 
 n, k = map(int, input().split())
