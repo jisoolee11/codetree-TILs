c13fc2e4ca32af217a670f084bd4b14c7837594a
Jisoo Lee <63850508+jisoolee11@users.noreply.github.com>
2024-03-09 11:57:03 +0900

    Title: 병원 거리 최소화하기, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240309/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -0,0 +1,44 @@
+def calc():
+    total = 0
+    for px, py in person:
+        min_h = 100
+        for hx, hy in selected_h:
+            dist = abs(hx-px) + abs(hy-py)
+            min_h = min(min_h, dist)
+        total += min_h
+    
+    return total
+
+def backtracking(cnt, idx):
+    global result
+
+    if cnt == m:
+        result = min(result, calc())
+        return
+
+    for i in range(idx, len(hospitals)):
+        if not visited[i]:
+            visited[i] = True
+            selected_h.append(hospitals[i])
+            backtracking(cnt+1, i+1)
+            visited[i] = False
+            selected_h.pop()
+
+
+n, m = map(int, input().split())
+person, hospitals = [], []
+for i in range(n):
+    row = list(map(int, input().split()))
+    for j in range(n):
+        if row[j] == 1:
+            person.append((i, j))
+        elif row[j] == 2:
+            hospitals.append((i, j))
+
+visited = [False] * len(hospitals)
+selected_h = []
+result = 100
+
+backtracking(0, 0)
+
+print(result)
